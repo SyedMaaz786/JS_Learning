a59ce668feac609fcbdfb248374ac12c65f5d94d
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-12 20:59:39 +0530

    REST API Integration

--- Rest_API_Integration/index.js
+++ Rest_API_Integration/index.js
@@ -53,6 +53,15 @@ app.get("/posts/:id", (req, res) => {
   res.render("show.ejs", { post });
 });
 
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  console.log(post);
+  res.send("patch request working!");
+});
+
 app.listen(port, () => {
   console.log("Listening to port: " + port);
 });
