f2286216661101c961a9c24c218ff8d04bd93ec8
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-12 19:45:14 +0530

    REST API integration

--- Rest_API_Integration/index.js
+++ Rest_API_Integration/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.send("server working well!");
+let posts = [
+  {
+    username: "Syed Maaz",
+    content: "Data Science",
+  },
+  {
+    username: "Salman Khan",
+    content: "Megastar",
+  },
+  {
+    username: "Roman Reigns",
+    content: "WWE",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
 });
 
 app.listen(port, () => {
