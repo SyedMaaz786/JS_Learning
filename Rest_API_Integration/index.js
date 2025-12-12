abf13a70211294fbb6156e184a9f339fbac5edd5
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-12 20:17:07 +0530

    REST API Integration

--- Rest_API_Integration/index.js
+++ Rest_API_Integration/index.js
@@ -36,7 +36,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
   let { username, content } = req.body; //destructuring the data present in the body
   posts.push({ username, content });
-  res.send("Post Request Working!");
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
